6db4a219b2e6db95ce518c9528456db2deca6a40
Анастасия5555555 <aleksandrprijmak577@gmail.com>
2023-09-08 12:17:25 +1000

    initial

---
+++ lesson.py
@@ -0,0 +1,15 @@
+# def count_letters(word):
+#     for letter in word: #мама
+#         counter = 0
+#         for let in word:
+#             if let == letter:
+#                 counter += 1
+#         print(letter, counter)
+# word = 'aacd'
+# count_letters(word)
+#
+# def count_letters(word): # решение N*M
+#     for letter in set(word): #мама
+#         counter = 0
+#         for let in word:
+#             if let == letter:
