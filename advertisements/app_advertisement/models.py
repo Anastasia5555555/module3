b5bd2786c75e070dd0794c981b6f5750da470a27
Anastasia5555555 <144233020+Anastasia5555555@users.noreply.github.com>
2023-10-14 23:31:04 +1000

    Add files via upload

--- advertisements/app_advertisement/models.py
+++ advertisements/app_advertisement/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Advertisement(models.Model):
+    # СharField - строковое поле для небольших строк
+    title = models.CharField('Заголовок', max_length=128)
+    # TextField - строковое поле для больших строк
+    description = models.TextField('Описание')
+    # 22300,55
+    # DecimalField - похоже на float в Питоне - числовое поле
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    # BooleanField - логическое поле, принимающее 2 значения: True/False
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг будет уместен')
+    # DateTimeField - cпециальное поле для работы с датами
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+    class Meta:
+        db_table = 'advertisements'
+    def __str__(self):
+        return f'Advertisement: Advertisement(id={self.id}, title={self.title},price={self.price})'
