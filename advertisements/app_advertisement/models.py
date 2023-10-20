66c83241992042a29f2553346446615cff9994cc
Anastasia5555555 <144233020+Anastasia5555555@users.noreply.github.com>
2023-10-20 20:49:30 +1000

    Add files via upload

--- advertisements/app_advertisement/models.py
+++ advertisements/app_advertisement/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 # Create your models here.
 class Advertisement(models.Model):
     # СharField - строковое поле для небольших строк
@@ -14,7 +15,28 @@ class Advertisement(models.Model):
     # DateTimeField - cпециальное поле для работы с датами
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', updated_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
     class Meta:
         db_table = 'advertisements'
     def __str__(self):
         return f'Advertisement: Advertisement(id={self.id}, title={self.title},price={self.price})'
+
